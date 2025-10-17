b119fc7c148d9634c765e997f5c614c6e0d4584f
Daniel Obando <oscar.obandov@upb.edu.co>
2025-10-17 15:16:36 -0500

    TRABAJO FINAL INICIO

---
+++ Trabajo Final/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Trabajo_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TRABAJO FINAL
+              INTEGRANTES:
+                -Aileen Sofía Graldo Andrade 
+                -Oscar Daniel Obando Vallejo 
+            */
+
+
+
+        }
+    }
+}
